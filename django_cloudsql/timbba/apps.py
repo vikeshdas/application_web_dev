a6ff8fdf8cd333094604e23625531af66f356eb9
Vikesh <Vikesh.das@subtlelabs.com>
2024-07-14 20:10:28 +0530

    fixed bug realted to duplicated timmba

--- django_cloudsql/timbba/apps.py
+++ django_cloudsql/timbba/apps.py
@@ -1,6 +1,13 @@
+# from django.apps import AppConfig
+
+
+# class ClientConfig(AppConfig):
+#     default_auto_field = "django.db.models.BigAutoField"
+#     name = "timbba"
 from django.apps import AppConfig
+import os
 
+class TimbbaConfig(AppConfig):
+    name = 'timbba'
+    path = os.path.join(os.path.dirname(os.path.dirname(__file__)), 'timbba')
 
-class ClientConfig(AppConfig):
-    default_auto_field = "django.db.models.BigAutoField"
-    name = "timbba"
--- django_cloudsql/timbba/settings.py
+++ django_cloudsql/timbba/settings.py
@@ -25,7 +25,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'timbba',
+    # 'timbba',
+    'timbba.apps.TimbbaConfig',
     'rest_framework',
     'rest_framework_simplejwt',
 ]
